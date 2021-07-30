12347bfcea842142a8bbe8757216d8ee1ce29af8
Teller35 <tellerwetzel@yahoo.com>
2021-07-30 11:15:00 -0600

    addUser, find User routes in place not tested

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,45 @@
-// import dependices for resolvers to work
+// import dependencies for resolvers to work
 const { User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
+const bookSchema = require("../models/Book");
 
 // create the resolvers
 const resolvers = {
-    Query: {
-        
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in!");
     },
-    Mutation: {
-
-    }
-}
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials!");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials!");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addBook: async (parent) => {},
+    removeBook: async (parent, args) => {
+      const deleteBook = await User.findOneAndUpdate(args);
+    },
+  },
+};
 
 module.exports = resolvers;
